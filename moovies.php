306437275b1ac4ee086be1bec647497c17b6fbcd
KaapFR <noaperrin69000@gmail.com>
2026-02-25 08:50:28 +0100

    stp

--- moovies.php
+++ moovies.php
@@ -1,3 +1,11 @@
 <?php
 
 $type = $_GET['type'] ?? 'popular';
+
+$typesAutorisés = ['popular', 'top_rated', 'upcoming', 'now_playing'];
+
+if (!in_array($type, $typesAutorisés)) {
+    http_response_code(400);
+    echo json_encode(["error" => "Type invalide. Utilise : popular, top_rated, upcoming ou now_playing"]);
+    exit;
+}
