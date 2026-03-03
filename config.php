4ece9913fe2e65fe8f605bcc7f477faa6e9e564a
Gabeshh <gabriel.laugier@ynov.com>
2026-03-03 20:25:07 +0100

    Clé API

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
 
-define('TMDB_API_KEY', getenv('TMDB_API_KEY') ?: 'CLE A METTRE ICI');
+define('TMDB_API_KEY', getenv('TMDB_API_KEY') ?: '651ef5a35841d032ea5d6f32e49d0019');
 define('TMDB_URL',     getenv('TMDB_BASE_URL') ?: 'https://api.themoviedb.org/3');
